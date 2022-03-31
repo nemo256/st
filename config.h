4cf8f71b9c0910f655f2231cdf9cfe87b1a14433
Amine Neggazi <neggazimedlamine@gmail.com>
2022-03-31 19:11:54 +0100

    Updated transparency effect!

--- config.h
+++ config.h
@@ -115,7 +115,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 1;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
