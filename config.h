90636bbe5f6caa34f53efcd4c101d7505491bf29
Amine Neggazi <neggazimedlamine@gmail.com>
2025-07-19 22:58:52 +0100

    MILESTONE - PIPI

--- config.h
+++ config.h
@@ -234,7 +234,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY ControlMask
+#define MODKEY Mod4Mask
 #define TERMMOD (Mod1Mask|ShiftMask)
 
 MouseKey mkeys[] = {
